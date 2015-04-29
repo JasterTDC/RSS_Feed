939ca55c26e0b2a5b68e0445f62c438e3684e2b7
Ismael Moral <jastertdc@icloud.com>
2015-04-29 13:33:59 +0200

    Added technology class

---
+++ EuropaPress/Technology.py
@@ -0,0 +1,34 @@
+
+# Include datetime library.
+import datetime
+
+# Technology class.
+class Tecnology:
+
+    # Class constructor.
+    def __init__(self, title, description, date, link, category):
+        self.title = title
+        self.description = description
+        self.date = date
+        self.link = link
+        self.category = category
+
+    # Title getter.
+    def getTitle () :
+        return self.title
+
+    # Description getter.
+    def getDescription () :
+        return self.description
+
+    # Date getter.
+    def getDate () :
+        return self.date
+
+    # Link getter.
+    def getLink () :
+        return self.link
+
+    # Category list getter.
+    def getCategory () :
+        return self.category
